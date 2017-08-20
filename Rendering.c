da6f7478e0648d923e771a1a1d4c960ed89580c1
Anton <shake3720@gmail.com>
2017-08-20 17:22:08 +0300

    Basic rendering implemented

--- Rendering.c
+++ Rendering.c
@@ -1,14 +1,54 @@
 #include <stdio.h>
 #include <curses.h>
 
+#include "Rendering.h"
+#include "Platform.h"
+#include "Player.h"
+
+
 bool render_all(int i, int y)
 {
     int ch;
+    platform my_plat = new_platform(3, 4);
+
+    render_static();
+
+    move(0, 0);
 
     if ((ch = getch()) != ERR)
         return false;
     else
-        printw("%d", i);
-    move(y, 0);
+        render_platform(my_plat);
     return true;
+}
+
+void render_static()
+{
+    move(WINDOW_Y, WINDOW_X);
+
+    for (int i = 0; i < WINDOW_WIDTH; i++)
+        addch('-');
+
+    for (int i = 1; i < WINDOW_HEIGHT; i++)
+    {
+        move(WINDOW_Y + i, WINDOW_X);
+        addch('|');
+
+        move(WINDOW_Y + i, WINDOW_X + WINDOW_WIDTH - 1);
+        addch('|');
+    }
+
+    move(WINDOW_Y + WINDOW_HEIGHT, WINDOW_X);
+
+    for (int i = 0; i < WINDOW_WIDTH; i++)
+        addch('-');
+
+}
+
+void render_platform(platform p)
+{
+    move(plnkh(p) + WINDOW_Y, plnkls(p) + WINDOW_X);
+
+    for (int i = 0; i < PLATFORM_WIDTH; i++)
+        addch('-');
 }
--- Rendering.h
+++ Rendering.h
@@ -1,5 +1,15 @@
 #ifndef __RENDERING__
 #define __RENDERING__
 
+#define WINDOW_X 3
+#define WINDOW_Y 3
+#define WINDOW_HEIGHT 20
+#define WINDOW_WIDTH 30
+
+#include "Platform.h"
+
 bool render_all(int i, int y);
+void render_static();
+void render_platform(platform p);
+
 #endif
