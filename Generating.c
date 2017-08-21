790e7d41d1678d60c730abbc433d8982fc88d6da
Evg-Mazay <evgenij997@yandex.ru>
2017-08-21 18:19:55 +0300

    platforms fixed

--- Generating.c
+++ Generating.c
@@ -8,7 +8,7 @@
 
 int random_int(const int min, const int max)
 {
-    return rand() % max + min;
+    return rand() % (max - min) + min;
 }
 
 void gen_chunk(platform* chunk)
@@ -16,7 +16,7 @@ void gen_chunk(platform* chunk)
 
     for (int i = 0; i < 3; ++i)
     {
-        chunk[i] = new_platform(random_int(0, WINDOW_WIDTH), random_int(0, WINDOW_HEIGHT));
+        chunk[i] = new_platform(random_int(0, WINDOW_WIDTH - PLATFORM_WIDTH), random_int(0, WINDOW_HEIGHT));
     }
 
 }
